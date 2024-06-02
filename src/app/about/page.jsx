f22b3b195a0dba3f3a5d5a1d91633c2dd1783c4b
Syeda Umme Ayman <aymanshoity@gmail.com>
2024-06-02 17:53:46 +0600

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Navbar from "./subcomponents/Navbar";
+
+const About = () => {
+   return (
+      <div>
+         <Navbar></Navbar>
+      </div>
+   );
+};
+
+export default About;
---
+++ src/app/about/subcomponents/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+   return (
+      <div  className="bg-red-500 h-10">
+         
+      </div>
+   );
+};
+
+export default Navbar;
